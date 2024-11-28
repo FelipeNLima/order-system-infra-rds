49169d232720f020ee704dd865cbe0104e6d76d0
FelipeNLima <felipinho.lima27@gmail.com>
2024-11-28 16:38:18 -0300

    fix: fixed

--- infra/rds/aws_rds.tf
+++ infra/rds/aws_rds.tf
@@ -11,8 +11,27 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_security_group" "banco_sg" {
+    name        = "banco_sg"
+    description = "Security Group for DB MySql"
+      ingress {
+      description = "MYSQL/Aurora"
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+      egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 resource "aws_db_instance" "banco" {
@@ -29,63 +48,7 @@ resource "aws_db_instance" "banco" {
     publicly_accessible  = true
     port                 = 3306
     vpc_security_group_ids = [aws_security_group.banco_sg.id]
-    db_subnet_group_name   = aws_db_subnet_group.subnet_payments.name
-    lifecycle {
-      ignore_changes = [snapshot_identifier]
-    }
     tags = {
       Name = "rdsDB"
     }
-}
-
-resource "aws_db_subnet_group" "subnet_payments" {
-  name        = "payments-db-subnet-group"
-  subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-}
-
-resource "aws_security_group" "banco_sg" {
-  name        = "Database Security Group"
-  description = "Security Group for DB MySql"
-  vpc_id      = aws_vpc.main.id
-    ingress {
-      description = "MYSQL/Aurora"
-      from_port   = 3306
-      to_port     = 3306
-      protocol    = "TCP"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-      egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "Main VPC"
-  }
-}
-
-resource "aws_subnet" "public_a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Public Subnet A"
-  }
-}
-
-resource "aws_subnet" "public_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Public Subnet B"
-  }
 }
